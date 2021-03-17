f6f24b7731573d2a97737a938e83b3743d2f948e
Samy Hajar <samy.hajar@gmail.com>
2021-03-17 11:48:19 +0100

    Add layout for generated PDF file

--- pages/test5.js
+++ pages/test5.js
@@ -13,6 +13,10 @@ export default function AppComponent() {
   const [plz, setPlz] = useState();
   const [staat, setStaat] = useState('');
   const [body, setBody] = useState('');
+  const [recipient, setRecipient] = useState('');
+  const [sender, setSender] = useState('');
+  const [dateTime, setDateTime] = useState();
+
   function generatePDF() {
     if (!process.browser) return;
 
@@ -21,18 +25,64 @@ export default function AppComponent() {
         // if you don't need styles, you can use a simple string to define a paragraph
 
         // using a { text: '...' } object lets you set styling properties
-        { text: name, fontSize: 15, margin: [0, 60, 0, 0] },
-        { text: address, fontSize: 15 },
+        { text: name, fontSize: 10, margin: [400, 2, 10, 0] },
+        {
+          text: address,
+          fontSize: 10,
+          style: 'header',
+          margin: [400, 2, 10, 0],
+        },
 
         // if you set the value of text to an array instead of a string, you'll be able
         // to style any part individually
-        { text: ort },
-        { text: plz },
-        { text: staat },
+        { text: ort, fontSize: 10, margin: [400, 2, 10, 0] },
+        { text: plz, fontSize: 10, margin: [400, 2, 10, 0] },
+        { text: staat, fontSize: 10, margin: [400, 2, 10, 10] },
+        {
+          canvas: [
+            {
+              type: 'line',
+              x1: 0,
+              y1: 5,
+              x2: 400 - 2 * 40,
+              y2: 5,
+              lineWidth: 0.5,
+            },
+          ],
+        },
+        { text: recipient, fontSize: 10, margin: [40, 10, 10, 0] },
+        {
+          canvas: [
+            {
+              type: 'line',
+              x1: 0,
+              y1: 5,
+              x2: 400 - 2 * 40,
+              y2: 5,
+              lineWidth: 0.5,
+            },
+          ],
+        },
+        { text: sender, fontSize: 15, margin: [40, 30, 10, 0], bold: false },
+        {
+          canvas: [
+            {
+              type: 'line',
+              x1: 0,
+              y1: 5,
+              x2: 400 - 2 * 40,
+              y2: 5,
+              lineWidth: 0.5,
+            },
+          ],
+        },
+        { text: dateTime, fontSize: 10, margin: [450, 30, 10, 0] },
+        { text: body, fontSize: 12, margin: [40, 40, 10, 0] },
       ],
     };
     const pdfDocGenerator = pdfMake.createPdf(docDefinition);
     pdfDocGenerator.getDataUrl(setUrl);
+    console.log(pdfDocGenerator);
   }
 
   return (
@@ -103,6 +153,8 @@ export default function AppComponent() {
             <form>
               <hr style={{ marginLeft: '20px', width: '50%' }} />
               <TextField
+                value={recipient}
+                onChange={(event) => setRecipient(event.currentTarget.value)}
                 size="small"
                 // className={classes.inputField}
                 style={{
@@ -118,6 +170,8 @@ export default function AppComponent() {
             <form>
               <hr style={{ marginLeft: '20px', width: '50%' }} />
               <TextField
+                value={sender}
+                onChange={(event) => setSender(event.currentTarget.value)}
                 style={{ marginLeft: '20px', marginTop: '40px' }}
                 id="outlined-multiline-static"
                 label="Sender"
@@ -130,6 +184,8 @@ export default function AppComponent() {
             </form>
             <form style={{ marginLeft: '550px', marginTop: '10px' }} noValidate>
               <TextField
+                value={dateTime}
+                onChange={(event) => setDateTime(event.currentTarget.value)}
                 id="date"
                 label="Datum"
                 type="date"
